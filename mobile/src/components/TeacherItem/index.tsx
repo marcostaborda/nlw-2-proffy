fb2c1f555752aedb1499332ce86de98d2374d090
Marcos Taborda <marcos.tabordamail@gmail.com>
2020-08-11 23:04:12 -0300

    Conexao post mobile

--- mobile/src/components/TeacherItem/index.tsx
+++ mobile/src/components/TeacherItem/index.tsx
@@ -7,6 +7,7 @@ import { RectButton } from "react-native-gesture-handler";
 import heartOutlineIcon from "../../assets/images/icons/heart-outline.png";
 import unFavoriteIcon from "../../assets/images/icons/unfavorite.png";
 import whatsappIcon from "../../assets/images/icons/whatsapp.png";
+import api from "../../services/api";
 
 export interface Teacher {
   id: string;
@@ -29,6 +30,9 @@ const TeacherItem: React.FC<TeacherItemProps> = ({ teacher, favorited }) => {
 
   function handleLinkToWhatsApp() {
     Linking.openURL(`whatsapp://send?phone=${teacher.whatsapp}`);
+    api.post("connections", {
+      user_id: teacher.id,
+    });
   }
   async function handleToggleFavorite() {
     const favorites = await AsyncStorage.getItem("favorites");
